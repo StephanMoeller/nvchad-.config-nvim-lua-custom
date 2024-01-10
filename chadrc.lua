88801357e8e266f001cc2349b73b56b88d61763f
Stephan <stephanryer@hotmail.com>
2024-01-10 19:55:39 +0100

    added chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,28 @@
 ---@type ChadrcConfig
 local M = {}
+M.ui = {
+    transparency = true,
+    theme = "tokyonight",
+    cmp = {
+        style = "atom",
+    },
+    tabufline = {
+        enabled = false,
+    },
+    statusline = {
+        theme = "minimal",
+        separator_style = "round",
+        overriden_modules = function(modules)
+            modules[#modules] = require("custom.configs.statusline").cursor_position()
+            -- modules[#modules + 1] = require("custom.configs.statusline").clock()
+            -- modules[#modules] = require("custom.configs.statusline").fileInfo()
+            table.remove(modules, 2)
+            table.insert(modules, 2, require("custom.configs.statusline").fileInfo())
 
-M.ui = { theme = 'onedark' }
-
+            -- table.insert(modules, 8, require("custom.configs.statusline").showcmd())
+        end,
+    },
+}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
 return M
